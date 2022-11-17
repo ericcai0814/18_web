6bda74189ef3bce27c1c26c95c16e0dbbfcd1476
Ericcai <d4070707@gmail.com>
2022-11-17 22:25:21 +0800

    project_1117

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
  //輪播圖
- var splide = new Splide('.splide', {
-    type: 'loop',
-    perPage: 3,
-    autoplay: true
-});
-var elms = document.getElementsByClassName('splide');
-for (var i = 0; i < elms.length; i++) {
-    new Splide(elms[i]).mount();
-}
+  
+    var splide = new Splide( '.splide', {
+        type  : 'fade',
+        rewind: true,
+    } );
+    
+    splide.mount();
+    
