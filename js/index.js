496f43510d5287ba15d09da9e7be831c481f91b9
Ericcai <d4070707@gmail.com>
2022-11-19 20:17:54 +0800

    project_1119_07

--- js/index.js
+++ js/index.js
@@ -3,9 +3,31 @@
 
 
 //輪播圖
-$(".slickContainer").slick({
+$(".slickContainer1").slick({
     dots: true,
     slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    responsive: [
+        {
+            breakpoint: 414,
+            settings: {
+                //arrows: false,
+                //centerMode: true,
+                centerPadding: '20px',
+                slidesToShow: 1
+            }
+        }
+        ],
+})
+
+
+/* 手機版輪播圖 */
+
+$(".slickContainer2").slick({
+    dots: true,
+    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
